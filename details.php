de8243cef8ee5e7bee6f6dcc462cc651f95075e0
viru2001 <vireshfegade@gmail.com>
2020-11-17 16:46:39 +0530

    completed booking page

--- details.php
+++ details.php
@@ -96,13 +96,13 @@
                     <!-- RD Mailform-->
                     <form class="form-fix"  action="" method="POST">
                         <div class="row row-20 row-fix">
-                        <div class="col-sm-12">
+                        <div class="col-sm-12 ">
                             <label class="form-label-outside">From</label>
                             <div  class="form-wrap form-wrap-inline">
-                            <select  id="source123" onchange="grabData()" class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="source">';
+                            <select  id="source123" onchange="getDates()"  class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="source">';
                             
-                             <!-- <option value="all">All</option>  -->
-                             <option value="" selected disabled hidden>Choose Source</option>
+                             <option value="all">Choose Source</option> 
+                             <!-- <option value="" selected disabled >Choose Source</option> -->
                             
                         
                             </select>
@@ -111,23 +111,25 @@
                         <div class="col-sm-12">
                             <label class="form-label-outside">To</label>
                             <div class="form-wrap form-wrap-inline">
-                            <select  class="form-input select-filter" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city" disabled>
+                            <select  class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="city" disabled>
                                 
                                 
                                 <option value="3"><?php echo "{$_SESSION['destination']}"; ?></option>
                             </select>
                             </div>
                         </div>
+                        <!-- <div class="col-sm-12 col-lg-6 re-render"> -->
                         <div class="col-sm-12 col-lg-6">
                             <label class="form-label-outside">Depart Date</label>
                             <div class="form-wrap form-wrap-validation">
                             <!-- Select -->
                             <!-- <input class="form-input" id="dateForm" name="date" type="text" > -->
                             <!-- <label class="form-label" for="dateForm">Choose the date</label> -->
-                            <select id="dates" class="form-input select-filter" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city">
+                            <select id="dates" onchange="getTime()" class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="city">
                                 
                                 <!-- <option value="4">4 days</option> -->
-                                <option value="" selected disabled hidden>Choose Dates</option>
+                                <option value="all" >Choose Date</option>
+                                <!-- <option value="" selected disabled hidden>Choose Dates</option> -->
                             </select>
                             
                             </div>
@@ -138,11 +140,11 @@
                         <div class="col-sm-12 col-lg-6">
                             <label class="form-label-outside">Depart Time</label>
                             <div class="form-wrap form-wrap-validation">
-                            <select id="time" class="form-input select-filter" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city">
+                            <select id="time" class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="city" >
                                 
                                 <!-- <option value="4">Choose time</option> -->
-
-                                <option value="" selected disabled hidden>Choose Time</option>
+                                <option value="all" >Choose Time</option>
+                                <!-- <option value="" selected disabled hidden>Choose Time</option> -->
                             </select>
                             <!-- <label class="form-label" for="dateForm">Choose the time</label> -->
                             </div>
@@ -153,25 +155,37 @@
                             <label class="form-label-outside">Duration</label>
                             <div class="form-wrap form-wrap-validation">
                             <!-- Select 2-->
-                            <select class="form-input select-filter" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city">
-                                
-                                <option value="4">4 days</option>
+                            <select   id="duration" class="form-input select-filter"  data-minimum-results-for-search="Infinity" name="city">
+                            <option value="all">Choose Duration</option>
+                                <!-- <option value="4">4 days</option> -->
                             </select>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <label class="form-label-outside">Adults</label>
                             <div class="form-wrap form-wrap-modern">
-                            <input class="form-input input-append" id="form-element-stepper" type="number" min="0" max="300" value="2">
+                            <input onchange="getAmount()"  class="form-input input-append passangers" id="form-element-stepper" type="number" min="1" max="100" value="1">
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <label class="form-label-outside">Children</label>
                             <div class="form-wrap form-wrap-modern">
-                            <input class="form-input input-append" id="form-element-stepper-1" type="number" min="0" max="300" value="0">
+                            <input onchange="getAmount()" class="form-input input-append passangers" id="form-element-stepper-1" type="number" min="0" max="100" value="0">
                             </div>
                         </div>
                         </div>
+
+                        <div class="col-sm-12 ">
+                            <label class="form-label-outside">Amount</label>
+                            <div class="form-wrap form-wrap-validation">
+                            <h3 id="amount" class="form-input "   name="city" >
+                                    ₹0
+                            </h3>
+                            <!-- <label class="form-label" for="dateForm">Choose the time</label> -->
+                            </div>
+                        </div>
+
+
                         <div class="form-wrap form-button">
                         <!-- <button class="button button-block button-secondary" name="submit1" type="submit">BOOK TOUR</button> -->
                         <button type="submit" name="submit1" class="button button-block button-secondary">BOOK TOUR</button>
@@ -208,28 +222,28 @@
         sel.appendChild(opt);
         }
         
-        document.getElementById("source123").value = 0;
+        // document.getElementById("source123").value = 0;
 
 
-        datesIndex = document.getElementById("source123").getAttribute('value');
-                if(datesIndex == null){
-                    datesIndex = 0;
-                }
-                console.log(datesIndex);
-                console.log(dates[datesIndex]);
-                var finalDates = dates[datesIndex].split(",");
+        // datesIndex = document.getElementById("source123").value;
+        //         if(datesIndex == null){
+        //             datesIndex = 0;
+        //         }
+        //         // console.log(datesIndex);
+        //         // console.log(dates[datesIndex]);
+        //         var finalDates = dates[datesIndex].split(",");
 
-                for(i=0;i<finalDates.length;i++){
+        //         for(i=0;i<finalDates.length;i++){
                 
-                var sel = document.getElementById('dates');
+        //         var sel = document.getElementById('dates');
                 
-                var opt = document.createElement('option');
+        //         var opt = document.createElement('option');
 
             
-                opt.appendChild( document.createTextNode(finalDates[i]) );
-                opt.value = finalDates[i]; 
-                sel.appendChild(opt);
-                }
+        //         opt.appendChild( document.createTextNode(finalDates[i]) );
+        //         opt.value = i; 
+        //         sel.appendChild(opt);
+        //         }
 
                
 
@@ -243,29 +257,10 @@
                 
                 
                 
-                timeIndex = document.getElementById("time").getAttribute('value');
-                if(timeIndex == null){
-                    timeIndex = 0;
-                }
-                // console.log(datesIndex);
-                // console.log(dates[datesIndex]);
-                var finalTime = departure[timeIndex].split(",");
 
-                for(i=0;i<finalTime.length;i++){
-                
-                var sel = document.getElementById('time');
-                
-                var opt = document.createElement('option');
+                function getDates(){
 
-            
-                opt.appendChild( document.createTextNode(finalTime[i]) );
-                opt.value = finalTime[i]; 
-                sel.appendChild(opt);
-                }
-
-                function grabData(){
-
-
+                    
                                 // document.getElementById("dates").
                                 $('#dates')
                                     .find('option')
@@ -273,12 +268,28 @@
                                     .end()
                                     
                                 ;
+                                var sel = document.getElementById('dates');
+
+                                var opt = document.createElement('option');
+                                // document.getElementsByClassName('re-render')[0].style.display=  "none";
+                                // document.getElementById('dates').style.display='none';
+
+
+                                opt.appendChild( document.createTextNode("Choose Date") );
+                                opt.value = "all"; 
+                                opt.selected = true;
+                                sel.appendChild(opt);
+
+                                // console.log(document.getElementById('dates').value);
+                                // console.log(document.getElementById('dates').selectedIndex);
+                                document.getElementById('dates').selectedIndex = 0;
+
                                 datesIndex = document.getElementById("source123").value;
                                 if(datesIndex == null){
                                     datesIndex = 0;
                                 }
-                                console.log(datesIndex);
-                                console.log(dates[datesIndex]);
+                                // console.log(datesIndex);
+                                // console.log(dates[datesIndex]);
                                 var finalDates = dates[datesIndex].split(",");
 
                                 for(i=0;i<finalDates.length;i++){
@@ -289,11 +300,39 @@
 
 
                                 opt.appendChild( document.createTextNode(finalDates[i]) );
-                                opt.value = finalDates[i]; 
+                                opt.value = i; 
                                 sel.appendChild(opt);
                                 }
-
                                 
+                                // document.getElementsByClassName('re-render')[0].style.display=  "";
+                                getDuration();
+                                getAmount();
+
+                }
+
+
+                
+                // timeIndex = document.getElementById("time").getAttribute('value');
+                // timeIndex = document.getElementById("dates").value;
+                // if(timeIndex == null){
+                //     timeIndex = 0;
+                // }
+                // // console.log(datesIndex);
+                // // console.log(dates[datesIndex]);
+                // var finalTime = departure[datesIndex].split(",");
+                // console.log("hi",finalTime);
+                // // for(i=0;i<finalTime.length;i++){
+                
+                // var sel = document.getElementById('time');
+                
+                // var opt = document.createElement('option');
+
+            
+                // opt.appendChild( document.createTextNode(finalTime[timeIndex]) );
+                // opt.value =timeIndex ; 
+                // sel.appendChild(opt);
+                // }
+                function getTime(){             
 
                         // document.getElementById("dates").
                         $('#time')
@@ -302,28 +341,121 @@
                             .end()
                             
                         ;
-                        timeIndex = document.getElementById("source123").value;
+                        var sel = document.getElementById('time');
+
+                                var opt = document.createElement('option');
+
+
+                                opt.appendChild( document.createTextNode("Choose Time") );
+                                opt.value = "all"; 
+                                sel.appendChild(opt);
+                        timeIndex = document.getElementById("dates").value;
                         if(timeIndex == null){
                             timeIndex = 0;
                         }
-                        console.log(timeIndex);
-                        console.log(departure[timeIndex]);
-                        var finalTime = departure[timeIndex].split(",");
-
-                        for(i=0;i<finalTime.length;i++){
+                        // console.log("timeIndex",timeIndex);
+                        // console.log("times",departure[datesIndex]);
+                        var finalTime = departure[datesIndex].split(",");
+                        // console.log("hiiii",finalTime);
+                        // for(i=0;i<finalTime.length;i++){
                         
                         var sel = document.getElementById('time');
                         
                         var opt = document.createElement('option');
 
                     
-                        opt.appendChild( document.createTextNode(finalTime[i]) );
-                        opt.value = finalTime[i]; 
+                        opt.appendChild( document.createTextNode(finalTime[timeIndex]) );
+                        opt.value = timeIndex; 
                         sel.appendChild(opt);
-                        }
+                        // }
+                        getAmount();
 
                 
                 }
+
+
+                function getDuration(){             
+
+
+                            $('#duration')
+                                .find('option')
+                                .remove()
+                                .end()
+                                
+                            ;
+                            var sel = document.getElementById('duration');
+
+                                    var opt = document.createElement('option');
+
+
+                                    opt.appendChild( document.createTextNode("Choose Duration") );
+                                    opt.value = "all"; 
+                                    sel.appendChild(opt);
+                            timeIndex = document.getElementById("dates").value;
+                            if(timeIndex == null){
+                                timeIndex = 0;
+                            }
+                            // console.log("timeIndex",timeIndex);
+                            // console.log("times",duration[datesIndex]);
+                            var Duration = duration[datesIndex].split(",");
+                            // console.log("Duration",Duration);
+                            // for(i=0;i<finalTime.length;i++){
+
+                            var sel = document.getElementById('duration');
+
+                            var opt = document.createElement('option');
+
+
+                            opt.appendChild( document.createTextNode(duration[datesIndex]) );
+                            opt.value = datesIndex; 
+                            sel.appendChild(opt);
+                            // }
+
+
+                            }
+
+                            
+
+                            function getAmount(){             
+
+
+                                    timeIndex = document.getElementById("dates").value;
+                                    if(timeIndex == null){
+                                        timeIndex = 0;
+                                    }
+                                    datesIndex = document.getElementById("source123").value;
+                                if(datesIndex == null){
+                                    datesIndex = 0;
+                                }
+                                console.log(availableSeats);
+                                var seats1 = availableSeats[datesIndex].split(",");
+                                console.log(seats1);
+                                var seats = parseInt(seats1[timeIndex]);
+                                
+                                console.log("seats" +seats);
+                                   
+                                    var adults = document.getElementsByClassName('passangers')[0].value ; 
+                                    var children = document.getElementsByClassName('passangers')[1].value ;
+
+                                    
+
+                                    var passangers = parseInt( adults) + parseInt(children ); 
+
+                                    // document.getElementsByClassName('passangers')[0].max = (seats - adults ).toString(); 
+                                    // document.getElementsByClassName('passangers')[1].max = (seats - children).toString() ;
+                                    // console.log(document.getElementsByClassName('passangers')[0].max,document.getElementsByClassName('passangers')[1].max);
+                                    // console.log(adults,children,passangers,prices[datesIndex]);
+                                    var x = document.getElementById("form-element-stepper").max = seats - children ;
+                                    var y = document.getElementById("form-element-stepper-1").max = seats - adults;
+                                    console.log(document.getElementById("form-element-stepper").max,document.getElementById("form-element-stepper-1").max)
+                                    document.getElementById('amount').innerHTML = "₹"+prices[datesIndex] * passangers;
+                                    
+                                    
+                                    
+                                   
+
+
+                            }
             
                 
 
