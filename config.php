5131af9dd4a216159889bf83228e802c43636d0a
viru2001 <vireshfegade@gmail.com>
2020-11-25 21:48:11 +0530

    security keys hided

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('611476881166-i3ekml57b2gsoa1gm3vfru204icntqof.apps.googleusercontent.com');
+$google_client->setClientId('xxxxxx.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('yNFw9zeXpVbCsPfIECnXzCy7');
+$google_client->setClientSecret('xxxxxxxxx');
 
 //Set the OAuth 2.0 Redirect URI
 $google_client->setRedirectUri('http://localhost/agency/signup.php');
